ea188e1cb259944b8657954b1a3c3bb8cd4b0cac
Juanchos2905 <juanchos2905@gmail.com>
2023-11-12 16:25:29 -0500

    Seeder initialization.

---
+++ BarberConect/DAL/SeederDB.cs
@@ -0,0 +1,35 @@
+﻿namespace BarberConect.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBaseContext _context;
+
+        public SeederDB(DataBaseContext context)
+        {
+            _context = context;
+        }
+/*
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            await PopulateAppointmentReservationsAsync();
+
+            await _context.SaveChangesAsync();
+        }
+
+        #region Private Methods
+        private async Task PopulateAppointmentReservationsAsync()
+        {
+            if (!_context.AppointmentRservation.Any())
+            {
+                _context.AppointmentRservations.Add(new AppointmentRservation
+                {
+
+                }
+            }
+        }
+            #endregion
+*/
+    }
+}
