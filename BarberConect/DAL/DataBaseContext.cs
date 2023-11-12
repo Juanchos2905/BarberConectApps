88c45df88ba1b0b94f5ef18f02af85715c949620
Juanchos2905 <juanchos2905@gmail.com>
2023-11-12 16:00:55 -0500

    Project structure

---
+++ BarberConect/DAL/DataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BarberConect.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ///base.OnModelCreating(modelBuilder);
+            ///modelBuilder.Entity<Hotel>().HasIndex(h => h.Name).IsUnique();
+            ///modelBuilder.Entity<Room>().HasIndex("Number", "HotelId").IsUnique();
+        }*/
+    }
+
+    //DBset!
+}
