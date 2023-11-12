0bd821a4c09524539782fb462aec6c842e3f213d
Juanchos2905 <juanchos2905@gmail.com>
2023-11-12 16:30:23 -0500

    AuditBase class was added

---
+++ BarberConect/DAL/Entities/AuditBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberConect.DAL.Entities
+{
+    public class AuditBase
+    {
+        [Key] //DataAnnotation
+        [Required] //doesn't allow null fields
+        public virtual Guid ID { get; set; }
+        public virtual DateTime? CreateDate { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
+
+    }
+}
